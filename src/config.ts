d07ad19e792c1ab3da1498c442f0911a2f8cc450
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-10-24 00:11:27 +0600

    Configure jwt and add zod validation middleware

--- src/config.ts
+++ src/config.ts
@@ -7,4 +7,10 @@ const pnv = process.env;
 export default {
   env: pnv.NODE_ENV,
   port: pnv.PORT,
+  jwt: {
+    jwt_secret: pnv.JWT_SECRET,
+    jwt_expires_in: pnv.JWT_EXPIRES_IN,
+    jwt_refresh_secret: pnv.JWT_REFRESH_SECRET,
+    jwt_refresh_expires_in: pnv.JWT_REFRESH_EXPIRES_IN,
+  },
 };
