cd5a11d079ef80191f3fbdc92f358a8ed8a48632
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-10-26 18:28:16 +0600

    Sign in query add in auth service

--- src/app/modules/auth/service.ts
+++ src/app/modules/auth/service.ts
@@ -2,6 +2,7 @@ import { User } from "@prisma/client";
 import prisma from "../../../constants/prisma";
 import ApiError from "../../../errors/ApiError";
 import { hashPassword } from "../../../helpers/bcrypt";
+import { ISingUp } from "./interface";
 
 const signUp = async (data: User): Promise<User> => {
   const isExist = await prisma.user.findFirst({
@@ -17,4 +18,11 @@ const signUp = async (data: User): Promise<User> => {
   return result;
 };
 
-export const AuthService = { signUp };
+const signIn = async ({ email, password }: ISingUp): Promise<User> => {
+  const result = await prisma.user.findFirst({ where: { email, password } });
+
+  if (!result) throw new ApiError(404, "User not found !!");
+  else return result;
+};
+
+export const AuthService = { signUp, signIn };
