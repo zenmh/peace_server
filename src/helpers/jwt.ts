d08445f2016d23aab61d0a63fa37620b8e484e92
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-10-26 20:14:25 +0600

    Add tow helpers to create access token and refresh token

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -1,4 +1,5 @@
 import { JwtPayload, Secret, sign, verify } from "jsonwebtoken";
+import config from "../config";
 
 const createToken = (
   payload: Record<string, unknown>,
@@ -12,4 +13,24 @@ const createToken = (
 const verifyToken = (token: string, secret: Secret): JwtPayload =>
   verify(token, secret) as JwtPayload;
 
-export { createToken, verifyToken };
+const createAccessToken = async (
+  userId: string,
+  role: string
+): Promise<string> =>
+  createToken(
+    { userId, role },
+    config.jwt.secret as Secret,
+    config.jwt.expires_in as string
+  );
+
+const createRefreshToken = async (
+  userId: string,
+  role: string
+): Promise<string> =>
+  createToken(
+    { userId, role },
+    config.jwt.refresh_secret as Secret,
+    config.jwt.refresh_expires_in as string
+  );
+
+export { createAccessToken, createRefreshToken, verifyToken };
