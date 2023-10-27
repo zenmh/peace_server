386f8e339ddd9d63003213f81b54148c6615441e
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-10-27 19:38:54 +0600

    Add treatment module in global router

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { AuthRoutes } from "./modules/auth/route";
 import { DoctorRoutes } from "./modules/doctor/route";
+import { TreatmentRoutes } from "./modules/treatment/route";
 
 const router = Router();
 
 [
   { path: "/auth", route: AuthRoutes },
   { path: "/doctors", route: DoctorRoutes },
+  { path: "/treatments", route: TreatmentRoutes },
 ].forEach(({ path, route }) => router.use(path, route));
 
 export const routes = router;
