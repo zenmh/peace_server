3635a538113d7e838a1b55870b69eaf369602647
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-10-28 18:51:39 +0600

    Move selected fildes in constant for doctor

---
+++ src/app/modules/doctor/constant.ts
@@ -0,0 +1,16 @@
+const select = {
+  id: true,
+  name: true,
+  email: true,
+  contactNo: true,
+  address: true,
+  image: true,
+  experiences: true,
+  speciality: true,
+  branch: true,
+  treatmentId: true,
+  createdAt: true,
+  updatedAt: true,
+};
+
+export { select };
--- src/app/modules/doctor/service.ts
+++ src/app/modules/doctor/service.ts
@@ -2,21 +2,7 @@ import { Doctor, Role } from "@prisma/client";
 import { hashPassword } from "../../../helpers/bcrypt";
 import prisma from "../../../constants/prisma";
 import ApiError from "../../../errors/ApiError";
-
-const select = {
-  id: true,
-  name: true,
-  email: true,
-  contactNo: true,
-  address: true,
-  image: true,
-  experiences: true,
-  speciality: true,
-  branch: true,
-  treatmentId: true,
-  createdAt: true,
-  updatedAt: true,
-};
+import { select } from "./constant";
 
 const createDoctor = async (data: Doctor): Promise<Doctor> => {
   let result;
