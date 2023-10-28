74cf2277460722d506f248cd24cdbf4cd2e3e671
mehedihasan2004 <thisismehedihasan0.1@gmail.com>
2023-10-28 18:16:40 +0600

    Protect create treatment route only for admin and super admin

--- src/app/modules/treatment/route.ts
+++ src/app/modules/treatment/route.ts
@@ -2,10 +2,18 @@ import { Router } from "express";
 import validateRequest from "../../middlewares/validateRequest";
 import { ZCreateTreatment } from "./validation";
 import { TreatmentController } from "./controller";
+import auth from "../../middlewares/auth";
+import { Role } from "@prisma/client";
 
 const router = Router();
+const { ADMIN, SUPER_ADMIN } = Role;
 const { createTreatment } = TreatmentController;
 
-router.post("/", validateRequest(ZCreateTreatment), createTreatment);
+router.post(
+  "/",
+  auth(ADMIN, SUPER_ADMIN),
+  validateRequest(ZCreateTreatment),
+  createTreatment
+);
 
 export const TreatmentRoutes = router;
